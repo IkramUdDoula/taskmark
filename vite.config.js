38c725f010d44cb0c5b3a2d9bc07e28ba8cc1753
IkramUdDoula <53461127+IkramUdDoula@users.noreply.github.com>
2025-06-14 15:57:34 +0600

    Update index.html and vite.config.js to enhance PWA support with new startup images and theme color

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'icons/*.png'],
       manifest: {
         name: 'TaskMark',
         short_name: 'TaskMark',
@@ -14,16 +15,29 @@ export default defineConfig({
         theme_color: '#111827',
         background_color: '#1A3636',
         display: 'standalone',
-        start_url: '.',
+        start_url: '/',
         icons: [
           {
-            src: 'icons/icon-512x512.png',
+            src: '/icons/icon-512x512.png',
             sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ],
+        screenshots: [
+          {
+            src: '/icons/splash-640x1136.png',
+            sizes: '640x1136',
             type: 'image/png'
           },
           {
-            src: 'icons/icon-512x512.png',
-            sizes: '512x512',
+            src: '/icons/splash-750x1334.png',
+            sizes: '750x1334',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/splash-1242x2208.png',
+            sizes: '1242x2208',
             type: 'image/png'
           }
         ]
